bc88e79d151aba83ec5a1c5f059e84d52599871e
Carl Damke <carl.simon.damke@gmail.com>
2016-05-01 14:31:29 +0200

    added comments and semicolons

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function startGame() {
 	myGameArea.start();
 }
-
+//field
 var myGameArea = {
 	canvas: document.createElement("canvas"),
 	circle: document.createElement("img"),
 	cross: document.createElement("img"),
 	start: function() {
-		this.circle.src = "kreis.png"
-		this.cross.src = "cross.png"
+		this.circle.src = "kreis.png";
+		this.cross.src = "cross.png";
 		this.canvas.width = 300;
 		this.canvas.height = 300;
 		this.context = this.canvas.getContext("2d");
@@ -22,17 +22,17 @@ var myGameArea = {
 		this.context.moveTo(0, 200);
 		this.context.lineTo(300, 200);
 		this.context.stroke();
-		var counter = 0
+		var counter = 0;
 		this.canvas.addEventListener("click", (event) => {
-			var x = Math.min(Math.floor(event.offsetX / 100), 2)
-			var y = Math.min(Math.floor(event.offsetY / 100), 2)
-			counter = counter + 1
+			var x = Math.min(Math.floor(event.offsetX / 100), 2);
+			var y = Math.min(Math.floor(event.offsetY / 100), 2);
+			counter = counter + 1;
 			if (counter % 2 === 0) {
-				this.context.drawImage(this.circle, x * 100, y * 100)
+				this.context.drawImage(this.circle, x * 100, y * 100);
 			} else {
-				this.context.drawImage(this.cross, x * 100, y * 100)
+				this.context.drawImage(this.cross, x * 100, y * 100);
 			}
 
-		})
+		});
 	}
-}
+};
